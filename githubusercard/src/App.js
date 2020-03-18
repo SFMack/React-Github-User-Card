e3df69fca6c94fe29d9c205b3c071df5122bbb66
SFMack <sfcamack@icloud.com>
2020-03-17 20:46:05 -0700

    followers array is being retrieved and set to state

--- githubusercard/src/App.js
+++ githubusercard/src/App.js
@@ -20,14 +20,28 @@ export default class App extends Component {
 			.catch(error => {
 				console.log('ERROR:: ', error);
 			});
+
+		axios
+			.get('https://api.github.com/users/SFMack/followers')
+			.then(response => {
+				this.setState({
+					followers: response.data
+				});
+			})
+			.catch(error => {
+				console.log('ERROR:: ', error);
+			});
 	};
 
 	render() {
 		const { data } = this.state;
+		const { followers } = this.state;
+
+		console.log(followers);
 
 		return (
 			<div className='App'>
-				<Card state={data} />
+				<Card state={data} followers={followers} />
 			</div>
 		);
 	}
--- githubusercard/src/Components/Card.js
+++ githubusercard/src/Components/Card.js
@@ -1,20 +1,35 @@
 import React from 'react';
 
 export default function Card(props) {
-	const { state } = props;
+	const { state, followers } = props;
 	console.log(state);
 
 	return (
 		<div className='card-wrapper'>
-			{state ? (
-				<>
-					<div>Login: {state.login}</div>
-					<div>Location: {state.location}</div>
-					<div>Bio: {state.bio} </div>
-					<div>Follower: {state.followers}</div>
-					<div>Following: {state.following}</div>
-				</>
-			) : null}
+			<div>
+				{state ? (
+					<>
+						<div>Login: {state.login}</div>
+						<div>Location: {state.location}</div>
+						<div>Bio: {state.bio} </div>
+						<div>Followers: {state.followers}</div>
+						<div>Following: {state.following}</div>
+					</>
+				) : null}
+			</div>
+			<div>
+				{followers
+					? followers.map(user => {
+							<div>
+								<div>Login: {user.login}</div>
+								<div>Location: {user.location}</div>
+								<div>Bio: {user.bio}</div>
+								<div>Followers: {user.followers}</div>
+								<div>Following: {user.following}</div>
+							</div>;
+					  })
+					: null}
+			</div>
 		</div>
 	);
 }
